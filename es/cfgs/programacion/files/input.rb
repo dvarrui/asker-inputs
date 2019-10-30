369932c77053acfe5a9445210e0cdaf7578ff2a7
David <dvarrui@protonmail.com>
2019-10-30 17:16:08 +0000

    U

---
+++ es/cfgs/programacion/files/input.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+print "Write your name: "
+name = gets.chop
+puts "Nice to meet you, #{name.capitalize}!"
---
+++ es/cfgs/programacion/rb-code-01.rb
@@ -0,0 +1,7 @@
+%map{ :version => '1', :lang => 'es', :context=>'ruby,lenguaje,programación' }
+
+  %code
+    %type ruby
+    %path files/input.rb
+    %tags recoger,input,dato,name,teclado,show,display,visualizar
+    %def Programa Ruby, que recoger un dato (name) por teclado y lo visualiza.
