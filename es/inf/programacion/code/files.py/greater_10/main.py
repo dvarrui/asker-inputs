278eef22f4e846e1948e58a9afb9ce267bb2c5fb
David <dvarrui@protonmail.com>
2019-11-11 09:43:10 +0000

    N greater_10

---
+++ es/inf/programacion/code/files.py/greater_10/main.py
@@ -0,0 +1,8 @@
+n1=int(input())
+n2=int(input())
+n3=int(input())
+
+if n1>10 or n2>10 or n3>10:
+	print("Exist value greater than 10")
+else:
+	print("None greater than 10")
---
+++ es/inf/programacion/code/files.rb/greater_10/main.rb
@@ -0,0 +1,9 @@
+n1 = ARGV[0].to_i
+n2 = ARGV[1].to_i
+n3 = ARGV[2].to_i
+
+if n1 > 10 || n2 > 10 || n3 > 10
+	puts("Exist value greater than 10")
+else
+	puts("None greater than 10")
+end
